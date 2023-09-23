314dc4be0eaf59a951b3fc5c1ec0522a47f9ddd8
Hyun Kyu Park <park620630@gmail.com>
2023-09-22 22:16:05 -0400

    first page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import TreeSample from './TreeSample.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [letters, show_num_letters] = useState('0')
+  let [d_day, change_dday] = useState('30')
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Color My Tree</h1>
+        <h2>Send your love to your friend</h2>
+      </div>
+      <div className='ImageContainer'>
+        <img src={TreeSample} alt="first page tree" width={400} />
+        <p>D-{d_day} until Christmas</p>
+        <button onClick={google_login} style={{fontSize:20}}>Continue with google account</button>
+      </div>
     </div>
   );
+}
+
+function google_login(){
+  console.log(1)
 }
 
 export default App;
